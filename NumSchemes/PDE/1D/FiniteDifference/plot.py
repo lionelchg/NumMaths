6478ed01263d7fd0e2b45a62debedbeb8a34063c
Lionel Cheng <cheng@cerfacs.fr>
2021-08-18 18:30:55 +0200

    Update plotting of 2D amplification factor

--- NumSchemes/PDE/1D/FiniteDifference/plot.py
+++ NumSchemes/PDE/1D/FiniteDifference/plot.py
@@ -106,44 +106,36 @@ def round_up(number: float, decimals:int=1) -> float:
     else:
         return 1.0
 
-def plot_ax_scalar(fig, ax, X, Y, field, title, cmap='RdBu', 
-        field_ticks=None, max_value=None, cbar=True):
+def plot_ax_scalar(fig, ax, X, Y, field, title):
     """ Plot a 2D field on mesh X and Y with contourf and contour. Automatic
     handling of maximum values for the colorbar with an up rounding to a certain
     number of decimals. """
     # If not specified find the maximum value and round this up to 1 decimal
-    if max_value is None:
-        max_value = round_up(np.max(np.abs(field)), decimals=1)
-    else:
-        max_value = round_up(max_value, decimals=1)
+    max_value = round_up(np.max(np.abs(field)), decimals=1)
 
     # Depending on the scale (log is typically for streamers) the treatment
     # is not the same
-    if cmap == 'Blues':
-        field_ticks = np.linspace(0, max_value, 5)
-        levels = np.linspace(0, max_value, 101)
-    else:
-        field_ticks = np.linspace(-max_value, max_value, 5)
-        levels = np.linspace(-max_value, max_value, 101)
-    cs1 = ax.contourf(X, Y, field, levels, cmap=cmap)
+    field_ticks = np.linspace(0, max_value, 5)
+    levels = np.linspace(0, max_value, 101)
+
+    cs1 = ax.contourf(X, Y, field, levels, cmap='Blues')
 
     # Contours
     clevels = np.linspace(np.min(field), np.max(field), 6)[1:-1]
     cs2 = ax.contour(X, Y, field, levels=clevels, colors='k', linewidths=0.9)
     ax.clabel(cs2, fmt='%.2f')
+    cs2 = ax.contour(X, Y, field, levels=[1.0], colors='y', linewidths=0.9)
+    ax.clabel(cs2, fmt='%.2f')
 
-    # Put colorbar if specified
+    # Adjust the size of the colorbar
     xmax, ymax = np.max(X), np.max(Y)
-    if cbar:
-        # Adjust the size of the colorbar
-        xmax, ymax = np.max(X), np.max(Y)
-        fraction_cbar = 0.1
-        aspect = 0.85 * ymax / fraction_cbar / xmax
-        # Set the colorbar in scientific notation
-        sfmt = mpl.ticker.ScalarFormatter(useMathText=True) 
-        sfmt.set_powerlimits((0, 0))
-        fig.colorbar(cs1, ax=ax, pad=0.05, fraction=fraction_cbar, aspect=aspect, 
-            ticks=field_ticks, format=sfmt)
+    fraction_cbar = 0.1
+    aspect = 7
+    # Set the colorbar in scientific notation
+    sfmt = mpl.ticker.ScalarFormatter(useMathText=True) 
+    sfmt.set_powerlimits((0, 0))
+    fig.colorbar(cs1, ax=ax, pad=0.05, fraction=fraction_cbar, aspect=aspect, 
+        ticks=field_ticks, format=sfmt)
 
     # Apply same formatting to x and y axis with scientific notation
     ax.ticklabel_format(axis='x', style='sci', scilimits=(0, 0))
@@ -167,7 +159,7 @@ def plot_G_2D(scheme, fig_dir):
                     cfl_1d[i], scheme), phi_1d, cfl_1d[i])
     
     # 2D plotting
-    fig, axes = plt.subplots(ncols=2, figsize=(8, 4))
-    plot_ax_scalar(fig, axes[0], phi, cfl, df_err, "Diffusion Error", cmap="Blues")
-    plot_ax_scalar(fig, axes[1], phi, cfl, dp_err, "Dispersion Error", cmap="Blues")
+    fig, axes = plt.subplots(ncols=2, figsize=(10, 4))
+    plot_ax_scalar(fig, axes[0], phi_deg, cfl, df_err, "Diffusion Error")
+    plot_ax_scalar(fig, axes[1], phi_deg, cfl, dp_err, "Dispersion Error")
     fig.savefig(fig_dir / f'errors_{scheme}', bbox_inches='tight')
