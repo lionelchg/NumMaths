ae5e55e49ac9b05a5812023d5e5f315f4cce8076
Guillaume Bogopolsky <bogopolsky@cerfacs.fr>
2022-04-16 19:11:54 +0200

    Add speedup plot

--- NumSchemes/ODE/libraries/cvode/vectorization/plot.py
+++ NumSchemes/ODE/libraries/cvode/vectorization/plot.py
@@ -23,16 +23,28 @@ for nloop, jac in tqdm(product(nloops, use_jac), total=len(nloops)*len(use_jac))
             data[jac]["vect"].append(float(line.split(" ")[1]))
             # print("Found vect in ", nloop, jac)
 
+# Convert to Numpy
+for jac in data:
+    for method in data[jac]:
+        data[jac][method] = np.asfarray(data[jac][method])
 
 # Plot
-fig, ax = plt.subplots(1, 1, figsize=(5,4))
+fig, axarr = plt.subplots(1, 2, figsize=(10,4))
+ax, ax2 = axarr.ravel()
+
 for jac in data:
     for method in data[jac]:
         ax.loglog(nloops, data[jac][method], label=f"{method} jac {jac}")
+    ax2.semilogx(nloops, data[jac]["serial"] / data[jac]["vect"], label=f"jac {jac}")
 ax.legend()
 ax.set_xlabel("nloops")
 ax.set_ylabel("t [s]")
 ax.set_title("Serial and vectorized solver performance")
+ax2.legend()
+ax2.set_xlabel("nloops")
+ax2.set_ylabel("speedup")
+ax2.set_title("Vectorized vs serial speedup")
+
 fig.tight_layout()
 fig.savefig("comp.png", dpi=150)
 os.system("imgcat comp.png")
